5cabc6895fe3aff669c40766f5465aa990782812
Karel Kohout <karel.kohout@post.cz>
2022-09-04 17:39:50 +0200

    úprava titulků

--- typescript/Titulky.ts
+++ typescript/Titulky.ts
@@ -9,6 +9,7 @@ export class Titulky {
         this.vyska = (pxVelikostCtverce + 2) * pocetRadku;
         this.sirka = (pxVelikostCtverce + 2) * pocetSloupcu;
         this.nastavRozmeryAPoziciElementu();
+        this.nastavVychoziFormatTextu();
         elPole.appendChild(this.elTitulky);
     }
     
@@ -19,34 +20,39 @@ export class Titulky {
         this.elTitulky.style.position = "absolute";
     }
     
+    private nastavVychoziFormatTextu(): void {
+        this.elTitulky.style.textAlign = "center";
+        this.elTitulky.style.fontWeight = "bold";
+        this.elTitulky.style.fontFamily = "Tahoma";
+        this.elTitulky.style.color = "white";
+    }
+    
     public async spustZaver(): Promise<void> {
         const elText = document.createElement("div");
-        elText.style.paddingTop = (this.vyska / 2) + "px";
-        elText.style.textAlign = "center";
-        elText.style.fontWeight = "bold";
-        elText.style.fontFamily = "Tahoma";
-        elText.style.color = "white";
-        elText.innerHTML = "KONEC";
         this.elTitulky.appendChild(elText);
+        elText.style.paddingTop = (this.vyska / 2) + "px";
+        elText.innerHTML = "KONEC";
         await this.zvetsujPismoElementu(elText);
     }
 
     public async spustUvod(): Promise<void> {
-        const elText = document.createElement("div");
-        elText.style.paddingTop = (this.vyska / 2) + "px";
-        elText.style.textAlign = "center";
-        elText.style.fontWeight = "bold";
-        elText.style.fontFamily = "Tahoma";
-        elText.style.color = "white";
-        elText.innerHTML = "TETRIS";
-        this.elTitulky.appendChild(elText);
-        await this.zvetsujPismoElementu(elText);
+        const elText1 = document.createElement("div");
+        const elText2 = document.createElement("div");
+        elText1.style.paddingTop = (this.vyska / 3) + "px";
+        elText2.style.paddingTop = (this.vyska / 5)  + "px";
+        elText1.innerHTML = "TETRIS";
+        elText2.innerHTML = "Karel Kohout";
+        this.elTitulky.appendChild(elText1);
+        await this.zvetsujPismoElementu(elText1);
+        this.elTitulky.appendChild(elText2);
+        await this.zvetsujPismoElementu(elText2, 1, 20, 2000);
     }    
 
     private async zvetsujPismoElementu(
         element: HTMLDivElement, 
         pocatecniVelikost: number = 1, 
-        konecnaVelikost: number = 30
+        konecnaVelikost: number = 30,
+        zastavitNaMs: number = 0
     ): Promise<void> {
         let velikostPisma = pocatecniVelikost;
         return await new Promise((konec) => {
@@ -55,7 +61,7 @@ export class Titulky {
                 if (velikostPisma < konecnaVelikost) {
                     setTimeout(zvetsiPismo, 100);
                 } else {
-                    setTimeout(() => konec(), 2000);
+                    setTimeout(() => konec(), zastavitNaMs);
                 }
             }
             zvetsiPismo();
